b28694d6509373b8b3cf1f4d07f3047a61df1fd5
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-05-23 22:15:11 -0600

    Add more specific for TS project

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          Or I will kill you
+          or not
         </p>
         <a
           className="App-link"
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,13 @@
 import { ethers } from "hardhat";
+import { PromiseOrValue } from "../typechain-types/common";
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+export default async function deploy(
+  arbiter: PromiseOrValue<string>,
+  beneficiary: PromiseOrValue<string>,
+  value: any) {
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  const Escrow = await ethers.getContractFactory("Escrow");
+  const escrow = await Escrow.deploy(arbiter, beneficiary, { value });
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  return escrow.deployed();
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
